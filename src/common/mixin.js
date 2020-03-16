f61312bcf4ab256a9e2b5d654516b637595bed68
wu zhanqin <1625494563@qq.com>
2020-03-16 12:04:48 +0800

    'mall'

---
+++ src/common/mixin.js
@@ -0,0 +1,38 @@
+import {debounce} from './utils'
+import BackTop from 'components/content/backTop/BackTop'
+
+export const itemListenerMixin = {
+	data(){
+		return {
+	  		itemImgListener:null,
+	  		refresh:null
+		}
+	},
+	mounted(){
+	  	this.refresh = debounce(this.$refs.scroll.refresh,50)
+	  	this.itemImgListener = () => {
+	  		this.refresh()
+	  	}
+	  	this.$bus.$on('itemImageLoad',this.itemImgListener)
+	  	console.log('我是混入中的内容');
+	  }
+}
+
+export const backTopMixin = {
+	components:{
+		BackTop
+	},
+	data(){
+		return {
+			isShowBackTop:false
+		}
+	},
+	methods:{
+		backTop(){
+			this.$refs.scroll.scrollTo(0,0,300)
+		},
+		listenShowBackTop(position){
+			this.isShowBackTop = (-position.y) > 1000
+		}
+	}
+}
